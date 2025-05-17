0ae90d41b7d4dc5c5d4dad5118e81f743d4e8b15
Maria Sojan <msojan@ucsd.edu>
2025-05-16 19:54:28 -0700

    gcd redo

--- designs/nangate45/gcd/configs/config_4_4_4.mk
+++ designs/nangate45/gcd/configs/config_4_4_4.mk
@@ -4,13 +4,13 @@ export PLATFORM    = nangate45
 export VERILOG_FILES = $(DESIGN_HOME)/src/$(DESIGN_NAME)/gcd.v
 export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
 export ABC_AREA      = 1
-export ABC_CLOCK_PERIOD_IN_PS = .41
 
 # Adders degrade GCD
 export ADDER_MAP_FILE :=
 
+export CORE_UTILIZATION ?= 55
 export CORE_UTILIZATION = 61.00
 export PLACE_DENSITY_LB_ADDON = 0.50
-export TNS_END_PERCENT        = 100
 export REMOVE_CELLS_FOR_EQY   = TAPCELL*
+export CORE_ASPECT_RATIO = 0.50
 export CORE_ASPECT_RATIO = 0.80
